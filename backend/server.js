9f5cc68782ec1c251942c625aa7e8f6d8f76f750
KOTAPHANITEJA <phanitejakota@gmail.com>
2025-04-28 15:46:49 +0530

    Render Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,6 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-app.listen(5000, () => {
-    connectDB();
-    console.log('Server is running on http://localhost:5000');
-});
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
